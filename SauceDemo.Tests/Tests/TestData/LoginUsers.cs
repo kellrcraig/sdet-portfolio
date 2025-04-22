de23157dddd4e58511486f0f9270d85f5aeab30d
Kelly Craig <kellrcraig@gmail.com>
2025-04-22 13:27:16 -0400

    test: Remove username constants and add username validation.

--- SauceDemo.Tests/Tests/TestData/LoginUsers.cs
+++ SauceDemo.Tests/Tests/TestData/LoginUsers.cs
@@ -1,14 +1,28 @@
-namespace SauceDemo.Tests.TestData
+
+namespace SauceDemo.Tests.Tests.TestData
 {
     public class LoginUsers
     {
         public const string CorrectPassword = "secret_sauce";
-        public const string IncorrectPassword = "secret_sauce";
-        public const string StandardUser = "standard_user";
-        public const string LockedOutUser = "locked_out_user";
-        public const string ProblemUser = "problem_user";
-        public const string PerformanceGlitchUser = "performance_glitch_user";
-        public const string ErrorUser = "error_user";
-        public const string VisualUser = "visual_user";
+        public const string IncorrectPassword = "secretsauce";
+        private static readonly HashSet<string> ValidUsers = new(StringComparer.OrdinalIgnoreCase)
+        {
+            "standard_user",
+            "locked_out_user",
+            "problem_user",
+            "performance_glitch_user",
+            "error_user",
+            "visual_user"
+        };
+
+        public static string ValidateUserName(string username)
+        {
+            if(!ValidUsers.Contains(username))
+            {
+                throw new ArgumentException(
+                    $"Invalid SauceDemo username: '{username}'. Allowed values: {string.Join(", ", ValidUsers)}");
+            }
+            return username;
+        }
     }
 }
