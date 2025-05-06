9c05ee1bfe3800457bc86b668190d4335d1b663a
kellrcraig <kellrcraig@gmail.com>
2025-05-06 18:45:32 -0400

    chore: Refactor BasePage for consistency with BaseComponent (#8)
    BasePage:
    - Convert driver field to protected property for consistency with BaseComponent
    - Remove FindElementSafe (now handled by extension methods)
    - Remove ElementIsVisible (no usages found)
    
    Affected Pages:
    - Replace FindElementSafe calls with extension method usage

--- SauceDemo.Tests/UI/Pages/BasePage.cs
+++ SauceDemo.Tests/UI/Pages/BasePage.cs
@@ -4,31 +4,11 @@ namespace SauceDemo.Tests.UI.Pages
 
     public abstract class BasePage
     {
-#pragma warning disable SA1401 // Fields should be private
-        protected readonly IWebDriver driver;
-#pragma warning restore SA1401 // Fields should be private
-
         public BasePage(IWebDriver driver)
         {
-            this.driver = driver;
+            Driver = driver;
         }
 
-        protected IWebElement? FindElementSafe(By locator)
-        {
-            try
-            {
-                return driver.FindElement(locator);
-            }
-            catch (NoSuchElementException)
-            {
-                return null;
-            }
-        }
-
-        protected bool ElementIsVisible(By locator)
-        {
-            var element = FindElementSafe(locator);
-            return element != null && element.Displayed;
-        }
+        protected IWebDriver Driver { get; }
     }
 }
