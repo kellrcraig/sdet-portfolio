5979d4142c8bcf1530dc2ffd307ce9d9dd79b903
kellrcraig <kellrcraig@gmail.com>
2025-05-07 13:23:20 -0400

    chore: Refactor model classes to records (#10)
    Simplifies PageModel and ProductNameModel by converting them to records and changing the logic of one property.

--- SauceDemo.Tests/Models/ProductNameModel.cs
+++ SauceDemo.Tests/Models/ProductNameModel.cs
@@ -1,25 +1,19 @@
 namespace SauceDemo.Tests.Models
 {
-    public sealed class ProductNameModel
+    public record ProductNameModel
     {
         public ProductNameModel(string displayName)
         {
             DisplayName = displayName;
-            InternalName = GetInternalName(displayName);
         }
 
         public string DisplayName { get; }
 
-        public string InternalName { get; }
-
-        private static string GetInternalName(string displayName)
-        {
-            return displayName
+        public string InternalName => DisplayName
                 .ToLowerInvariant()
                 .Replace(" ", "-")
                 .Replace(".", string.Empty)
                 .Replace("(", string.Empty)
                 .Replace(")", string.Empty).ToLower();
-        }
     }
 }
